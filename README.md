21c1e95bad490a9d8ab0532dab3f1d220f6f1233
Maxim Oransky <maxim.oransky@gmail.com>
2020-09-16 15:46:35 +0300

    examples/message

--- README.md
+++ README.md
@@ -4,10 +4,26 @@ Simple client library.
 
 ## Examples
 
-Server version:
+### Server version
 
 ```
 go run examples/serverinfo/main.go
 go run examples/serverinfo/main.go -verbose
+go run examples/serverinfo/main.go -verbose -server https://demo.tada.team
 ```
 
+### Messaging
+
+```
+go run examples/message/main.go -team <team uid> -token <bot token> -chat <chat jid> -message <message text>
+```
+
+For example:
+
+```https://demo.tada.team/dbd248d7-25c2-4e8f-a23a-99baf63223e9/chats/g-dce6f5fd-b741-40a6-aa9c-c0e928d9dac5```
+
+ * https://demo.tada.team — server url
+ * dbd248d7-25c2-4e8f-a23a-99baf63223e9 – team uid
+ * g-dce6f5fd-b741-40a6-aa9c-c0e928d9dac5 – chat jid
+ 
+How to get bot token: type "/newbot <NAME>" command in @TadaBot direct chat
--- examples/message/main.go
+++ examples/message/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
 	"time"
 
 	"github.com/tada-team/tdclient"
@@ -9,41 +10,51 @@ import (
 )
 
 func main() {
-	// your team uid
-	team := "uid"
+	server := flag.String("server", "https://web.tada.team", "server address")
+	team := flag.String("team", "", "team uid")
+	chat := flag.String("chat", "", "chat jid")
+	token := flag.String("token", "", "bot token. Type \"/newbot <NAME>\" command in @TadaBot direct chat")
+	message := flag.String("message", "test message", "message text")
+	verbose := flag.Bool("verbose", false, "verbose logging")
+	flag.Parse()
 
-	// group / direct / task chat identifier
-	chat := "chat jid"
+	if *token == "" {
+		fmt.Println("-token required")
+		return
+	}
 
-	// bot token. Type "/newbot <NAME>" command in @TadaBot direct chat
-	token := "secret"
+	if *chat == "" {
+		fmt.Println("-chat required")
+		return
+	}
 
-	// make http session
-	client, err := tdclient.NewSession("https://web.tada.team")
+	if *team == "" {
+		fmt.Println("-team required")
+		return
+	}
+
+	client, err := tdclient.NewSession(*server)
 	if err != nil {
 		panic(err)
 	}
 
-	client.SetToken(token)
+	client.SetToken(*token)
+	client.SetVerbose(*verbose)
 
-	// enable logging
-	client.SetVerbose(true)
-
-	// getting server settings, for example
-	features, err := client.Features()
-	if err
-	log.Println("server version:", features.Build)
-
-	// websocket connection
-	ws, err := client.Ws(team, nil)
+	websocketConnection, err := client.Ws(*team, nil)
 	if err != nil {
 		panic(err)
 	}
+
+	recipient := *tdproto.NewJID(*chat)
 
 	// composing like human. Full events list at https://github.com/tada-team/tdproto
-	ws.Send(tdproto.NewClientChatComposing(jid, true, nil))
+	websocketConnection.Send(tdproto.NewClientChatComposing(recipient, true, nil))
 	time.Sleep(3 * time.Second)
 
 	// shortcut for simple messaging
-	ws.SendPlainMessage(chat, "hello, world")
+	websocketConnection.SendPlainMessage(recipient, *message)
+	if _, err := websocketConnection.WaitForConfirm(); err != nil {
+		panic(err)
+	}
 }
