c7643853801f67ad0ab3d80ed2896e51c4c6f308
igo95862 <igo95862@yandex.ru>
2021-10-27 13:55:52 +0300

    Print stack traces of errors in unit tests

--- session_test.go
+++ session_test.go
@@ -18,18 +18,18 @@ func TestSession(t *testing.T) {
 
 	s, err := NewSession(testServer)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("%+v", err)
 	}
 
 	t.Run("http ping", func(t *testing.T) {
 		if err := s.Ping(); err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 	})
 
 	t.Run("features smoke test", func(t *testing.T) {
 		if _, err := s.Features(); err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 	})
 
@@ -38,7 +38,7 @@ func TestSession(t *testing.T) {
 	t.Run("sms login", func(t *testing.T) {
 		codeResp, err := s.AuthBySmsSendCode(testAccountPhone)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 
 		if codeResp.CodeLength != len(testAccountCode) {
@@ -47,7 +47,7 @@ func TestSession(t *testing.T) {
 
 		tokenResp, err := s.AuthBySmsGetToken(testAccountPhone, testAccountCode)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 
 		//if len(tokenResp.Me.Teams) == 0 {
@@ -67,7 +67,7 @@ func TestSession(t *testing.T) {
 	if team.Uid == "" {
 		team, err = s.createTeam(tdapiTeam{Name: "tdclient test"})
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 		log.Println("new team created:", team.Uid)
 	}
@@ -77,12 +77,12 @@ func TestSession(t *testing.T) {
 		anyPhone := "+79870000000"
 		newContact, err = s.AddContact(team.Uid, anyPhone)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 
 		contacts, err := s.Contacts(team.Uid)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 
 		newContactFound := false
@@ -99,7 +99,7 @@ func TestSession(t *testing.T) {
 	t.Run("messages", func(t *testing.T) {
 		message, err := s.SendPlaintextMessage(team.Uid, newContact.Jid, kozma.Say())
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 
 		if message.Chat != newContact.Jid {
@@ -112,7 +112,7 @@ func TestSession(t *testing.T) {
 			filter.Limit = 200
 			messages, err := s.GetMessages(team.Uid, newContact.Jid, filter)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 			if len(messages) < 1 {
 				t.Error("invalid get messages:", len(messages))
@@ -122,7 +122,7 @@ func TestSession(t *testing.T) {
 		t.Run("delete messages", func(t *testing.T) {
 			_, err := s.DeleteMessage(team.Uid, newContact.Jid, message.MessageId)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 		})
 
@@ -131,7 +131,7 @@ func TestSession(t *testing.T) {
 	t.Run("me smoke test", func(t *testing.T) {
 		me, err := s.Me(team.Uid)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 		if !me.CanAddToTeam {
 			t.Fatal("cant add to team")
@@ -140,17 +140,17 @@ func TestSession(t *testing.T) {
 
 	t.Run("ws", func(t *testing.T) {
 		ws, err := s.Ws(team.Uid, func(err error) {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		})
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 
 		t.Run("ping", func(t *testing.T) {
 			confirmId := ws.Ping()
 			ev := new(tdproto.ServerConfirm)
 			if err := ws.WaitFor(ev); err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 			if ev.Params.ConfirmId != confirmId {
 				t.Error("confirmId mismatched: got:", ev.ConfirmId, "want:", confirmId)
@@ -161,7 +161,7 @@ func TestSession(t *testing.T) {
 			messageUid := ws.SendPlainMessage(newContact.Jid, kozma.Say())
 			msg, _, err := ws.WaitForMessage()
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 			if msg.MessageId != messageUid {
 				t.Fatal("invalid message uid")
@@ -171,7 +171,7 @@ func TestSession(t *testing.T) {
 				ws.DeleteMessage(messageUid)
 				msg, _, err := ws.WaitForMessage()
 				if err != nil {
-					t.Fatal(err)
+					t.Fatalf("%+v", err)
 				}
 				if msg.MessageId != messageUid {
 					t.Fatal("invalid message uid")
@@ -180,7 +180,7 @@ func TestSession(t *testing.T) {
 
 			t.Run("close", func(t *testing.T) {
 				if err := ws.Close(); err != nil {
-					t.Fatal("close ws session fail")
+					t.Fatalf("%+v", err)
 				}
 			})
 		})
@@ -197,7 +197,7 @@ func TestSession(t *testing.T) {
 			RemindAt:    tdproto.IsoDatetime(time.Now().Add(time.Minute)),
 		})
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 		if task.Description != text {
 			t.Error("task description mismatched: want:", text, "got:", task.Description)
@@ -212,7 +212,7 @@ func TestSession(t *testing.T) {
 			},
 		})
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 
 		if len(chats) <= 2 {
@@ -232,13 +232,13 @@ func TestSession(t *testing.T) {
 			Public:      false,
 		})
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("%+v", err)
 		}
 
 		t.Run("add member", func(t *testing.T) {
 			member, err := s.AddGroupMember(team.Uid, group.Jid, newContact.Jid)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 			if member.Status != tdproto.GroupMember {
 				t.Error("invalid status:", member.Status)
@@ -248,7 +248,7 @@ func TestSession(t *testing.T) {
 		t.Run("get members", func(t *testing.T) {
 			members, err := s.GroupMembers(team.Uid, group.Jid)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 			if len(members) != 2 {
 				t.Error("invalid groups number:", len(members))
@@ -257,20 +257,20 @@ func TestSession(t *testing.T) {
 
 		t.Run("remove member", func(t *testing.T) {
 			if err := s.DropGroupMember(team.Uid, group.Jid, newContact.Jid); err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 		})
 
 		t.Run("remove group", func(t *testing.T) {
 			if err := s.DropGroup(team.Uid, group.Jid); err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 		})
 
 		t.Run("group list smoke test", func(t *testing.T) {
 			_, err := s.GetGroups(team.Uid)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("%+v", err)
 			}
 		})
 	})
